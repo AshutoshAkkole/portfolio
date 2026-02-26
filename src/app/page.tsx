0e5712cd656ba2920cf465692310296a97fff1bc
Ashutosh Akkole <akkoleashutosh01@gmail.com>
2026-02-27 01:08:53 +0530

    Redirect to portfolio on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+"use client";
+
 import Link from "next/link";
+import { useLayoutEffect } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const isRedirection = true;
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    isRedirection && router.push("/portfolio");
+  }, [])
+
+  if (isRedirection) {
+    return null;
+  }
+
   return(
     <>
     <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
